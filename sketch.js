8b8744ebc88809578af5e0f17873951ed37c7571
Mazin Chabayta <mazin.chabayta@gmail.com>
2018-11-05 20:08:49 -0500

    sliders for rgb and pot for geo

--- sketch.js
+++ sketch.js
@@ -12,12 +12,15 @@ var serial;       //variable to hold the serial port object
 var sensorValue0;      //this variable will hold the value from "s1"
 var sensorValue1;      //this variable will hold the value from "s2"
 var sensorValue2;
+var sensorValue3;
+var sensorValue4;
+var sensorValue5;
 
-var serialPortName = "/dev/cu.usbmodem1431";  //FOR PC it will be COMX on mac it will be something like "/dev/cu.usbmodemXXXX"
+var serialPortName = "/dev/cu.usbmodem1421";  //FOR PC it will be COMX on mac it will be something like "/dev/cu.usbmodemXXXX"
                               //Look at P5 Serial to see the available ports
 function setup() {
   
-  createCanvas(1000,1000);
+  createCanvas(windowWidth, windowHeight);
   //Setting up the serial port
   serial = new p5.SerialPort();     //create the serial port object
   serial.open(serialPortName); //open the serialport. determined 
@@ -25,12 +28,28 @@ function setup() {
   serial.on('data',dataReceived);   //when data is received execute the dataReceived function
 }
 
-function draw() {  
-    fill(sensorValue0,sensorValue1,sensorValue2);
-    ellipse(width/2,height/2, width/2, height/2); //apply the sensor value to the radius of the ellipse
-  
-  
+function draw() 
+{  
+    background(sensorValue3,sensorValue4,sensorValue5);  
+    
+    push();
+    translate(width*0.8, height*0.5);
+    rotate(frameCount / sensorValue1);
+    polygon(0,0, sensorValue2, sensorValue0); 
+    pop();
 }
+
+function polygon(x, y, radius, npoints) {
+  var angle = TWO_PI / npoints;
+  beginShape();
+  for (var a = 0; a < TWO_PI; a += angle) {
+    var sx = x + cos(a) * radius;
+    var sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
+
 
 
 function dataReceived()   //this function is called every time data is received
@@ -41,9 +60,12 @@ var rawData = serial.readStringUntil('\r\n'); //read the incoming string until i
     if(rawData.length>1)                      //check that there is something in the string
     {                                         
       
-      sensorValue0 = JSON.parse(rawData).s0;       //the parameter value .s1 must match the parameter name created within the arduino file
-      sensorValue1 = JSON.parse(rawData).s1; 
-    sensorValue2 = JSON.parse(rawData).s2; 
+    sensorValue0 = JSON.parse(rawData).s0;       //the parameter value .s1 must match the parameter name created within the arduino file
+    sensorValue1 = JSON.parse(rawData).s1; 
+    sensorValue2 = JSON.parse(rawData).s2;
+    sensorValue3 = JSON.parse(rawData).s3;
+    sensorValue4 = JSON.parse(rawData).s4;
+    sensorValue5 = JSON.parse(rawData).s5;
     }
 }
 
